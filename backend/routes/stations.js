0fab94e357a4a3c5a0315a72fabac8d50c20ba19
AdrianFreshman <70104863+AdrianFreshman@users.noreply.github.com>
2023-11-05 12:41:39 +0100

    Revert "Dev"

--- backend/routes/stations.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const stations = require("../models/stations.js");
-
-router.get("/", (req, res) => stations.getStationNames(req, res));
-
-module.exports = router;
