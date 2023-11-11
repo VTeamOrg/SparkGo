7f44349b5617287a8325e8fed673d2a88ea367c5
blixtenA <anbx22@bth.se>
2023-11-11 12:52:14 -0500

    Adapted DB to fit ER chart, added base data

--- backend/sql/ddl.sql
+++ backend/sql/ddl.sql
@@ -12,53 +12,89 @@ drop table if exists company;
 drop table if exists city;
 
 -- create schema for e-scooter database
-create table company (
-    id int primary key,
-    name varchar(255),
-    number varchar(255),
-    employees int
-);
-
 create table city (
     id int primary key,
     name varchar(255)
 );
 
-create table number (
-    embg varchar(255) primary key,
-    phone_number varchar(255) unique
+CREATE TABLE frequencies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
-create table renting_station (
-    id int primary key,
-    name varchar(255),
-    number_e_scooter int,
-    company_have_r_station int,
-    r_station_located_in int,
-    foreign key (company_have_r_station) references company(id),
-    foreign key (r_station_located_in) references city(id)
+CREATE TABLE renting_station (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    city_id INT,
+    name VARCHAR(255),
+    coords_lat DECIMAL(10, 6), 
+    coords_long DECIMAL(10, 6), 
+    FOREIGN KEY (city_id) REFERENCES city(id)
 );
 
-create table member (
-    embg varchar(255) primary key,
-    roles int,
-    firstname varchar(255),
-    lastname varchar(255),
-    date_born date,
-    phone_number varchar(255),
-    foreign key (phone_number) references number(phone_number)
+CREATE TABLE member (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    role VARCHAR(255),
+    email VARCHAR(255),
+    name VARCHAR(255),
+    personal_number VARCHAR(255)
+    /* wallet??? */
 );
 
-create table scooter (
+CREATE TABLE vehicle (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    city_id INT,
+    type VARCHAR(255),
+    rented_by INT,
+    FOREIGN KEY (city_id) REFERENCES city(id),
+    FOREIGN KEY (rented_by) REFERENCES member(id)
+);
+
+CREATE TABLE plan (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    description TEXT,
+    price DECIMAL(10, 2),
+    price_frequency_id INT,
+    included_unlocks INT,
+    included_unlocks_frequency_id INT,
+    included_minutes INT,
+    included_minutes_frequency_id INT,
+    FOREIGN KEY (price_frequency_id) REFERENCES frequencies(id),
+    FOREIGN KEY (included_unlocks_frequency_id) REFERENCES frequencies(id),
+    FOREIGN KEY (included_minutes_frequency_id) REFERENCES frequencies(id)
+);
+
+CREATE TABLE active_plan (
+    plan_id INT,
+    member_id INT,
+    activation_date DATE,
+    available_minutes INT,
+    available_unlocks INT,
+    is_paused ENUM('Y', 'N'),
+    PRIMARY KEY (plan_id, member_id, activation_date),
+    FOREIGN KEY (plan_id) REFERENCES plan(id),
+    FOREIGN KEY (member_id) REFERENCES member(id)
+);
+
+CREATE TABLE receipt (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    member_id INT,
+    payment_details VARCHAR(255),
+    payment_type VARCHAR(255),
+    receipt_details VARCHAR(255)
+);
+
+
+/*create table scooter (
     id int primary key,
     country varchar(255),
     price decimal(10, 2),
     status varchar(255),
     scooter_is_on_station int,
     foreign key (scooter_is_on_station) references renting_station(id)
-);
+);*/
 
-create table rent (
+/*create table rent (
     id int primary key,
     rented_at datetime,
     returned_at datetime,
@@ -70,10 +106,10 @@ create table rent (
     foreign key (r_station_rented_from) references renting_station(id),
     foreign key (r_station_returned_to) references renting_station(id),
     foreign key (scooter_rented_scooter) references scooter(id)
-);
+); */
 
 -- create triggers for the scooter table
-drop trigger if exists log_scooter_insert;
+/*drop trigger if exists log_scooter_insert;
 drop trigger if exists log_scooter_update;
 
 delimiter //
@@ -94,6 +130,6 @@ begin
     values (concat('scooter details for scooter id ', new.id, ' updated.'));
 end//
 
-delimiter ;
+delimiter ; */
 
 show tables;
