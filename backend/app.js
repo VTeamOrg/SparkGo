387c4a3a971c3ccb74d6f59237236d643f036cd9
asili2001 <ahmadasili1928@gmail.com>
2023-12-04 23:41:10 +0100

    Restructured route files, removed body-parser and used express.json insead, implemented basic websocket functionality

--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,25 @@
 require("dotenv").config();
 
 const express = require("express");
+const http = require('http');
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const websocket = require('ws');
+const loadHttpRoutes = require("./routes/httpRoutes");
+const loadWebsocketRoutes = require('./routes/websocketRoutes');
 
-const users = require("./routes/users.js");
-const stations = require("./routes/stations.js");
-const vehicles = require("./routes/vehicles.js");
-const cities = require("./routes/cities.js");
 
 const app = express();
-const httpServer = require("http").createServer(app);
+const server = http.createServer(app);
+const wss = new websocket.Server({ server });
+
 
 app.use(cors());
 app.options("*", cors());
 
 app.disable("x-powered-by");
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-const io = require("socket.io")(httpServer, {
-    cors: {
-        origin: [
-            "http://localhost:9000",
-            "http://localhost:1337",
-            "http://localhost:1337/users",
-            "http://localhost:1337/stations",
-            "http://localhost:1337/cities",
-            "http://localhost:1337/vehicles",
-            "http://127.0.0.1:3306",
-            "http://localhost:5137"
-        ],
-        methods: ["GET", "POST"],
-    },
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 const port = process.env.NODE_ENV === "test" ? 1337 : 3000; // Use port 1337 for testing
 
@@ -44,12 +29,10 @@ app.get("/", (req, res) => {
     });
 });
 
-app.use("/users", users);
-app.use("/stations", stations);
-app.use("/vehicles", vehicles);
-app.use("/cities", cities);
+loadHttpRoutes(app);
+loadWebsocketRoutes(wss);
 
-httpServer.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 
--- backend/routes/httpRoutes/cities.js
+++ backend/routes/httpRoutes/cities.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const cityModule = require("../models/cities.js");
+const cityModule = require("../../models/cities.js");
 
 /* GET routes */
 router.get("/", (req, res) => cityModule.getAllCities(req, res));
