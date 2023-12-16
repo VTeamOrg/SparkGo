1a17944add97b212c2ebd878b6e59b3e49e24247
asili2001 <ahmadasili1928@gmail.com>
2023-12-16 01:44:34 +0100

    Added paymentMethods routes

--- backend/routes/httpRoutes/index.js
+++ backend/routes/httpRoutes/index.js
@@ -2,12 +2,16 @@ const users = require("./users.js");
 const stations = require("./stations.js");
 const vehicles = require("./vehicles.js");
 const cities = require("./cities.js");
+const subscription = require("./subscription.js");
+const paymentMethods = require("./paymentMethods.js");
 
 const loadHttpRoutes = (app)=> {
     app.use("/users", users);
     app.use("/stations", stations);
     app.use("/vehicles", vehicles);
     app.use("/cities", cities);
+    app.use("/subscription", subscription);
+    app.use("/paymentMethods", paymentMethods);
 }
 
 module.exports = loadHttpRoutes;
