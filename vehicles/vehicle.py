a60b9661c7631ceb68da2dbf81ed42ee64059c8d
asili2001 <ahmadasili1928@gmail.com>
2023-12-23 04:54:25 +0100

    Created the Vehicle class designed for integration into physical vehicles. This class seamlessly connects to the server, continuously updating it with real-time vehicle data.

--- vehicles/vehicle.py
+++ vehicles/vehicle.py
@@ -1,47 +1,11 @@
 import random
 import asyncio
-import signal
 import websockets
 import multiprocessing
 import json
-import requests
 import logging
 
 
-
-"""
-Vehicle class
-    - vehicle_id: int
-    - battery: int
-    - bounds: [int, int, int, int]
-    - lat: int
-    - lng: int
-
-    THe class represents a vehicle object. It has the following properties:
-    - vehicle_id: The ID of the vehicle
-    - battery: The battery level of the vehicle
-    - bounds: The bounds of the vehicle
-    - lat: The latitude of the vehicle
-    - lng: The longitude of the vehicle
-    - max_speed: The maximum speed of the vehicle
-    - is_started: Whether the vehicle is started or not
-    - rented_by: The user ID of the user that rented the vehicle
-    - current_speed: The current speed of the vehicle
-    - websocket: The websocket connection of the vehicle
-    - activate(): A method to activate the vehicle
-    - run_vehicle_status(): A method to run the vehicle status
-    - connect_websocket(): A method to connect to the websocket
-    - send_message(): A method to send a message to the websocket
-    - receive_message(): A method to receive a message from the websocket
-    - print_vehicle_status(): A method to print the vehicle status
-    - rent_vehicle(): A method to rent the vehicle
-    - return_vehicle(): A method to return the vehicle
-    - start_vehicle(): A method to start the vehicle
-    - stop_vehicle(): A method to stop the vehicle
-    - calculate_speed(): A method to calculate the speed of the vehicle
-    - decrease_battery(): A method to decrease the battery of the vehicle
-    - reconnect_websocket(): A method to reconnect the websocket connection if it is lost
-"""
 class Vehicle:
     def __init__(self, vehicle_id, battery, bounds, lat, lng):
         self.vehicle_id = vehicle_id
@@ -59,33 +23,15 @@ class Vehicle:
     async def activate(self):
         await self.connect_websocket()
         asyncio.create_task(self.run_vehicle_status())
+        asyncio.create_task(self.process_websocket_messages())
         asyncio.create_task(self.calculate_speed())
         asyncio.create_task(self.decrease_battery())
         asyncio.create_task(self.reconnect_websocket())
 
-
-    # Print the vehicle status
-    async def print_vehicle_status(self):
-        print(f"""
-            Vehicle Status:
-            Vehicle ID: {self.vehicle_id}
-            Battery: {self.battery.value}
-            Bounds: {self.bounds}
-            Location: {self.lat}, {self.lng}
-            Max Speed: {self.max_speed.value}
-            Is Started: {self.is_started.value}
-            Rented By: {self.rented_by.value}
-        """)
-
     # Run the vehicle status
     async def run_vehicle_status(self):
         while True:
-            # await self.print_vehicle_status()
             await self.send_message("updateVehicleStatus")
-            # response = await self.receive_message()
-            # if response:
-            #     with self.max_speed.get_lock():
-            #         self.max_speed.value = random.randint(30, 60)
             await asyncio.sleep(5)
 
 
@@ -115,7 +61,7 @@ class Vehicle:
                 break
 
     # Send a message to the websocket
-    async def send_message(self, action):
+    async def send_message(self, action, payload = None):
         try:
             payload = {
                 "action": action,
@@ -126,7 +72,8 @@ class Vehicle:
                 "maxSpeed": self.max_speed.value,
                 "isStarted": self.is_started.value,
                 "rentedBy": self.rented_by.value,
-                "currentSpeed": self.current_speed.value
+                "currentSpeed": self.current_speed.value,
+                "payload": payload
             }
             await self.websocket.send(json.dumps(payload))
             logging.info(f"Sent: {json.dumps(payload)}")
@@ -150,13 +97,52 @@ class Vehicle:
         except Exception as e:
             logging.error(f"Unexpected error in receiving message: {e}")
     
+    # a method to always listen to the websocket server responses and update the vehicle status
+    async def process_websocket_messages(self):
+        while True:
+            response = await self.receive_message()
+            if response:
+                response = json.loads(response)
+                print("Response---------------------->: ", response)
+                if not response["action"]:
+                    continue
+                
+                if response["action"] == "startVehicle":
+                        self.is_started.value = 1
+                        print(f"Vehicle {self.vehicle_id} started by user {self.rented_by.value}.\n {response['message']}")
+                elif response["action"] == "stopVehicle":
+                    self.is_started.value = 0
+                    self.current_speed.value = 0
+                    print(f"Vehicle {self.vehicle_id} stopped by user {self.rented_by.value}.\n {response['message']}")
+                elif response["action"] == "returnVehicle":
+                    self.rented_by.value = -1
+                    print(f"Vehicle {self.vehicle_id} returned by user {self.rented_by.value}.\n {response['message']}")
+                
+                elif response["action"] == "rentVehicle":
+                    self.rented_by.value = response["rentedBy"]
+                    print(f"Vehicle {self.vehicle_id} rented by user {self.rented_by.value}.\n {response['message']}")
+                
+                elif response["action"] == "maxSpeed":
+                    self.max_speed.value = response["maxSpeed"]
+                    print(f"Vehicle {self.vehicle_id} max speed updated.\n {response['message']}")
+                
+                elif response["action"] == "warning":
+                    print(f"Vehicle {self.vehicle_id} warning.\n {response['message']}")
+
+                elif response["action"] == "error":
+                    print(f"Vehicle {self.vehicle_id} error.\n {response['message']}")
+
+
+            await asyncio.sleep(5)
+
     # Rent the vehicle
     async def rent_vehicle(self, user_id):
         if self.rented_by.value == -1:
-            self.rented_by.value = user_id
             action = "rentVehicle"
-            await self.send_message(action)
-            print(f"Vehicle {self.vehicle_id} rented by user {user_id}")
+            payload = {
+                "userId": user_id
+            }
+            await self.send_message(action, payload)
             return True
         else:
             print(f"Vehicle {self.vehicle_id} is already rented.")
@@ -165,12 +151,8 @@ class Vehicle:
     # Return the vehicle
     async def return_vehicle(self, user_id):
         if self.rented_by.value == user_id:
-            self.is_started.value = 0
-            self.rented_by.value = -1
-            self.current_speed.value = 0
             action = "returnVehicle"
             await self.send_message(action)
-            print(f"Vehicle {self.vehicle_id} returned by user {user_id}")
             return True
         else:
             print(f"Vehicle {self.vehicle_id} can't be returned. Not rented by user {user_id}.")
@@ -179,10 +161,8 @@ class Vehicle:
     # Start the vehicle
     async def start_vehicle(self, user_id):
         if self.rented_by.value == user_id and not self.is_started.value:
-            self.is_started.value = 1
-            action = "start"
+            action = "startVehicle"
             await self.send_message(action)
-            print(f"Vehicle {self.vehicle_id} started by user {user_id}")
             return True
         else:
             print(f"Vehicle {self.vehicle_id} can't be started by user {user_id}.")
@@ -191,10 +171,8 @@ class Vehicle:
     # Stop the vehicle
     async def stop_vehicle(self):
         if self.is_started.value:
-            self.is_started.value = 0
-            action = "stop"
+            action = "stopVehicle"
             await self.send_message(action)
-            print(f"Vehicle {self.vehicle_id} stopped")
             return True
         else:
             return False
@@ -218,144 +196,3 @@ class Vehicle:
                     self.battery.value -= 1
                     print("Decreasing battery: ", self.battery.value)
             await asyncio.sleep(30)
-
-
-
-"""
-    The following code is for vehicle class testing purposes
-"""
-
-# Function to generate a path between two points with smaller steps
-async def move_towards_destination(vehicle, end_point):
-    start_point = [vehicle.lat, vehicle.lng]
-    def generate_path(start_point, end_point, num_steps):
-        path = []
-        for i in range(num_steps + 1):
-            fraction = i / num_steps
-            intermediate_point = [
-                start_point[0] + (end_point[0] - start_point[0]) * fraction,
-                start_point[1] + (end_point[1] - start_point[1]) * fraction,
-            ]
-            path.append(intermediate_point)
-        return path
-    
-    path = generate_path(start_point, end_point, 10) # 10 steps between two points
-
-    print("------------->, generated paths: ", path)
-    for point in path:
-        if vehicle.is_started.value:
-            print("Moving to: ", point)
-            vehicle.lat = point[0]
-            vehicle.lng = point[1]
-            await asyncio.sleep(1)
-        
-    await vehicle.stop_vehicle()
-
-
-vehicles = []
-
-
-def getServerVehicles():
-    url = "http://localhost:3000/vehicles"
-    response = requests.get(url)
-    return response.json()
-
-def getServerUsers():
-    url = "http://localhost:3000/users"
-    response = requests.get(url)
-    return response.json()
-
-
-async def create_vehicles():
-    server_vehicles = getServerVehicles()
-    vehicle_ids = [item['id'] for item in server_vehicles['data']]
-    for id in vehicle_ids:
-        random_start_point = [random.randint(0, 100), random.randint(0, 100)]
-        vehicle = Vehicle(id, multiprocessing.Value('i', 100), [0, 0, 0, 0], random_start_point[0], random_start_point[1])
-        vehicles.append({id: id, "vehicle": vehicle})
-        await vehicle.activate()
-        # await vehicle.rent_vehicle(id * 2)
-        # await vehicle.start_vehicle(id * 2)
-
-    # asyncio.create_task(move_towards_destination(vehicle, [90, 40]))  # Run this in the background
-
-async def run_vehicles():
-    server_vehicles = getServerVehicles()
-    server_users = getServerUsers()
-    user_ids = [item['id'] for item in server_users['data']]
-    vehicle_ids = [item['id'] for item in server_vehicles['data']]
-    print("Server vehicles: ", vehicle_ids)
-
-    def get_random_user():
-        return random.choice(user_ids)
-    
-    def get_vehicle(user_id):
-        vehicle = None
-        for item in vehicles:
-            if item["vehicle"].rented_by.value == user_id:
-                vehicle = item["vehicle"]
-                break
-
-        if vehicle is None:
-            vehicle = random.choice(vehicles)["vehicle"]
-
-        return vehicle
-    
-    def get_random_point():
-        return [random.randint(0, 100), random.randint(0, 100)]
-    
-    def get_random_action_for_user(user_id, vehicle):
-        print("-------------------->", vehicle.rented_by.value, user_id)
-        if vehicle.rented_by.value == user_id:
-            if vehicle.is_started.value:
-                choices = ["stop", "drive"]
-                probabilities = [0.2, 0.8]
-                return random.choices(choices, probabilities)[0]
-            
-            else:
-                choices = ["start", "return"]
-                probabilities = [0.8, 0.2]
-                return random.choices(choices, probabilities)[0]
-        else:
-            return random.choice(["rent"])
-    
-    while True:
-        user_id = get_random_user()
-        vehicle = get_vehicle(user_id)
-        
-        action = get_random_action_for_user(user_id, vehicle)
-
-        print("User: ", user_id, "Vehicle: ", vehicle.vehicle_id, "Action: ", action)
-
-        if action == "rent":
-            await vehicle.rent_vehicle(user_id)
-        elif action == "return":
-            await vehicle.return_vehicle(user_id)
-        elif action == "start":
-            await vehicle.start_vehicle(user_id)
-        elif action == "drive":
-            await move_towards_destination(vehicle, get_random_point())
-        elif action == "stop":
-            await vehicle.stop_vehicle()
-        await asyncio.sleep(5)
-
-async def main():
-    try:
-        await create_vehicles()
-        await run_vehicles()
-    except Exception as e:
-        logging.error("Exception: ", e)
-        # Proper cleanup routines here for any open resources
-        # ...
-    finally:
-        for vehicle in vehicles:
-            await vehicle.stop_vehicle()
-            await vehicle.return_vehicle(vehicle.rented_by.value)
-        logging.info("Exiting...")
-
-    while True:
-        await asyncio.sleep(3600)  # Keep the event loop running indefinitely
-
-if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal.SIG_DFL) # Allow Ctrl-C to exit the program
-    asyncio.run(main())
