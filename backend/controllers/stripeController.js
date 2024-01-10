cab9aa6202dbea0f230ee356e77067928ed624dc
Louise <lojn22@student.bth.se>
2024-01-10 23:45:30 +0100

    stripe payment

---
+++ backend/controllers/stripeController.js
@@ -0,0 +1,33 @@
+const stripe = require('stripe')(process.env.STIPE_SECRET_KEY);
+const subscriptionModule = require("../models/subscription");
+
+const stripeController = {
+    session:async (req, res)=> {
+        const session = await stripe.checkout.sessions.create({
+            mode: 'subscription',
+            line_items: [
+              {
+                price: 'price_1OUVhgErt9JzUGzKViVbb3i4',
+                quantity: 1,
+              },
+            ],
+            ui_mode: 'embedded',
+            return_url: 'http://localhost:3000/subscription/process?session_id={CHECKOUT_SESSION_ID}',
+          }); 
+        return res.json(session);
+    },
+    process:async (req, res) => {
+      const {session_id} = req.query
+      const checkout_session = await stripe.checkout.sessions.retrieve(session_id)
+      console.log(checkout_session)
+      // res.json({status:'success'})
+      await subscriptionModule.updateSubscriptionInDatabase(checkout_session);
+      res.writeHead(302, {
+          'Location': process.env.WEBAPP_URL
+        });
+        res.end();
+    },
+    
+}
+
+module.exports = stripeController;
