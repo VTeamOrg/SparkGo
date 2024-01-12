a9a14489904acb73cc10e31ad713536ad6a7b979
asili2001 <ahmadasili1928@gmail.com>
2024-01-12 10:51:00 +0100

    removed unnecessary files

--- backend/routes/httpRoutes/activePlan.js
+++ backend/routes/httpRoutes/activePlan.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const activePlanModule = require("../../models/activePlan.js");
+const activePlanModel = require("../../models/activePlanModel.js");
 
 /* GET routes */
-router.get("/", (req, res) => activePlanModule.getAllActivePlans(req, res));
-router.get("/:activePlanId", (req, res) => activePlanModule.getActivePlanById(req, res));
+router.get("/", (req, res) => activePlanModel.getAllActivePlans(req, res));
+router.get("/:activePlanId", (req, res) => activePlanModel.getActivePlanById(req, res));
 
-router.post("/", (req, res) => activePlanModule.createActivePlan(req, res));
+router.post("/", (req, res) => activePlanModel.createActivePlan(req, res));
 
-router.put("/:activePlanId", (req, res) => activePlanModule.updateActivePlan(req, res));
+router.put("/:activePlanId", (req, res) => activePlanModel.updateActivePlan(req, res));
 
-router.delete("/:activePlanId", (req, res) => activePlanModule.deleteActivePlan(req, res));
+router.delete("/:activePlanId", (req, res) => activePlanModel.deleteActivePlan(req, res));
 
 module.exports = router;
